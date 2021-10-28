48dd0acfbe9579d4ceb2185e841b054d9ce0a763
Eghinner <eghinner@gmail.com>
2021-10-28 09:07:28 -0700

    add dependency emotionstyled

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
+
+const ContenedorHeader = styled.header`
+
+`
+
+const Header = ({titulo}) => {
+	return (
+		<header>
+			<h1>{titulo}</h1>
+		</header>
+	)
+}
+
+Header.propTypes = {
+	titulo: PropTypes.string.isRequired,
+}
+
+export default Header
